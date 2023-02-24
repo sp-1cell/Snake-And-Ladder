dcfd767bb19660000664f43448f25156cec428fe
sp-1cell <sushant6247@gmail.com>
2023-02-24 20:19:13 +0530

    CODE ADDED FOR :GAME PLAYED WITH SINGLE PLAYER AT START POSITION 0

---
+++ SnakeLadderUC1.java
@@ -0,0 +1,13 @@
+package Snake_Ladder;
+
+public class SnakeLadderUC1 {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Snake Ladder program :");
+
+        int position = 0;
+        System.out.println("Player is at Position = " + position);
+    }
+}
+
+
