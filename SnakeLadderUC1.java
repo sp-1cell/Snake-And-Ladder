9f9160243f9646c8b8a16e8743c798bea2286481
sp-1cell <sushant6247@gmail.com>
2023-02-25 11:43:27 +0530

    checks for a position

--- SnakeLadderUC1.java
+++ SnakeLadderUC1.java
@@ -2,12 +2,33 @@ package Snake_Ladder;
 
 public class SnakeLadderUC1 {
     public static void main(String[] args) {
-
-        System.out.println("Welcome to Snake Ladder program :");
-
         int position = 0;
-        System.out.println("Player is at Position = " + position);
+        System.out.println("Player is at Position " + position);
+
+        int randomDiceNumber = (int) Math.floor(Math.random() * 100) % 6 + 1;
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        // System.out.println(" Option " + option);
+
+        switch (option) {
+            case 1:
+                // snake
+                position = position - randomDiceNumber;
+                if (position < 0) {
+                    position = 0;
+                }
+                break;
+            case 2:
+                // ladder
+                position = position + randomDiceNumber;
+            default:
+                // no play
+                position = position;
+                break;
+        }
+
+        System.out.println("final Position " + position);
     }
 }
+
 
 
